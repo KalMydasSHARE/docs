39f2075521d80aa880178883b25ab65c09f4605c
kalmydas-cloud <kalmydas@gmail.com>
2026-07-15 01:56:06 +0200

    chat: disclaimer permanent non repliable dans l'espace de discussion (L4-e)

--- _next/static/chunks/app/strategies/layout-1d3233299487cc34.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[9689,3610,1321,2472,419,9227,9554,7167,2991,5428,7381,4227,2467,9532],{3521:function(){}},function(n){n.O(0,[2971,2117,1744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
