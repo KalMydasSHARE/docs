07bf9c1589e9ed753ea7fc80c74138e2a481620f
kalmydas-cloud <kalmydas@gmail.com>
2026-07-18 22:47:44 +0200

    ok

--- _next/static/chunks/app/bridge-usdc-base/layout-a86b2716d7738fae.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[2472,419,3610,1321,9227,7167,9689,9554,4227,2991,2467,7381,9532,5428],{3521:function(){}},function(n){n.O(0,[2971,2117,1744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
