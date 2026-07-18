e5ab12c61289f04c5df993402abae74aed15b6a7
kalmydas-cloud <kalmydas@gmail.com>
2026-07-18 18:29:40 +0200

    ok

--- _next/static/chunks/app/wallet-setup/layout-14cc2414d175f6fc.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[5428,1321,419,9554,9227,3610,2472,7167,9689,2991,7381,2467,4227,9532],{3521:function(){}},function(n){n.O(0,[2971,2117,1744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
