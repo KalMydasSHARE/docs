d6c2a4b39a9eb2b348b5ddb817f5d1347612795a
kalmydas-cloud <kalmydas@gmail.com>
2026-07-20 23:45:52 +0200

    docs(legal): ajout de la rubrique Historique des versions des CGU, FR et EN

--- _next/static/chunks/app/audits/layout-9396c57e3f0baf29.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[9227,1321,2472,9554,3610,419,2991,9689,7381,7167,4227,2467,5428,9532],{3521:function(){}},function(n){n.O(0,[2971,2117,1744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
---
+++ _next/static/chunks/app/strategies/layout-a402a7701fafea77.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[9689,1321,419,7167,9554,2472,2991,9227,3610,5428,4227,2467,7381,9532],{3521:function(){}},function(n){n.O(0,[2971,2117,1744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
