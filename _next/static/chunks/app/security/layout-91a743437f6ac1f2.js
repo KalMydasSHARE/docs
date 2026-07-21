c0f3752ed92f5992ece20ebf84b68b573e0f7dff
kalmydas-cloud <kalmydas@gmail.com>
2026-07-21 22:10:28 +0200

    d

---
+++ _next/static/chunks/app/security/layout-91a743437f6ac1f2.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[9532,419,3610,9227,1321,2472,9689,9554,7167,2991,4227,7381,5428,2467],{3521:function(){}},function(n){n.O(0,[2971,2117,1744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
--- _next/static/chunks/app/why-kalmydas/layout-dd5efe28b59f8dac.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[7381,9227,419,3610,2472,1321,9554,7167,9689,2991,4227,2467,5428,9532],{3521:function(){}},function(n){n.O(0,[2971,2117,1744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
